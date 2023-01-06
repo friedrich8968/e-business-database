5dfac5f999a08c6fca4a1f10f7f211b239b58237
friedrich <friedrich@hp-envy.fritz.box>
2023-01-06 21:48:34 +0100

    zweite Datenbank für die Benutzer hinzugefügt

--- database.py
+++ database.py
@@ -17,7 +17,7 @@ db = SQLAlchemy(app)
 # Initialize marshmallow
 ma = Marshmallow(app)
 
-# Product model
+# Project Datenbank
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
@@ -56,7 +56,48 @@ class ProjectSchema(ma.Schema):
 product_schema = ProjectSchema()
 products_schema = ProjectSchema(many=True)
 
-# Create a project
+#User Datenbank
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    javascript = db.Column(db.Boolean(100))
+    webdesign = db.Column(db.Boolean)
+    textilarbeit = db.Column(db.Boolean)
+    produktdesign = db.Column(db.Boolean)
+    holzarbeit = db.Column(db.Boolean)
+    druck = db.Column(db.Boolean)
+    software = db.Column(db.Boolean)
+    goldschmied = db.Column(db.Boolean)
+
+def __init__(self, name, email, javascript, webdesign, textilarbeit,
+             produktdesign, holzarbeit, druck, software, goldschmied):
+    self.name = name
+    self.email = email
+    self.javascript = javascript
+    self.webdesign = webdesign
+    self.textilarbeit = textilarbeit
+    self.produktdesign = produktdesign
+    self.holzarbeit = holzarbeit
+    self.druck = druck
+    self.software = software
+    self.goldschmied = goldschmied
+
+# User Schema
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'email', 'javascript', 'webdesign', 'textilarbeit',
+                  'produktdesign', 'holzarbeit', 'druck', 'software', 'goldschmied')
+
+# Initialize project schema
+user_schema = UserSchema()
+user_schema = UserSchema(many=True)
+
+
+
+
+# GET, POST Methoden
+# Methode um ein neues Projekt anzulegen
 @app.route('/project', methods=['POST'])
 def add_project():
     name = request.json['name']
@@ -71,10 +112,32 @@ def add_project():
     goldschmied = request.json['goldschmied']
     description = request.json['description']
 
-    new_product = Product(name, email, javascript, webdesign, textilarbeit,
+    new_project = Product(name, email, javascript, webdesign, textilarbeit,
                           produktdesign, holzarbeit, druck, software, goldschmied, description)
 
-    db.session.add(new_product)
+    db.session.add(new_project)
     db.session.commit()
 
-    return ProjectSchema.jsonify(new_product)
+    return ProjectSchema.jsonify(new_project)
+
+#Methode um einen neuen User anzulegen
+@app.route('/anmelden', methods=['POST'])
+def add_project():
+    name = request.json['name']
+    email = request.json['email']
+    javascript = request.json['javascript']
+    webdesign = request.json['webdesign']
+    textilarbeit = request.json['textilarbeit']
+    produktdesign = request.json['produktdesign']
+    holzarbeit = request.json['holzarbeit']
+    druck = request.json['druck']
+    software = request.json['software']
+    goldschmied = request.json['goldschmied']
+
+    new_user = Product(name, email, javascript, webdesign, textilarbeit,
+                          produktdesign, holzarbeit, druck, software, goldschmied)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return UserSchema.jsonify(new_user)
